31ecd01816000ba5835aa4495c29667266594668
TTsdzb <ttsdzb@outlook.com>
2023-10-18 22:09:31 +0800

    feat(types): add stats type
    Add types for chart stats.

--- types/__init__.py
+++ types/__init__.py
@@ -1,1 +1,2 @@
 from music import Music, MusicBasicInfo, MusicChart
+from stats import Stats, StatsChart, StatsDiff
---
+++ types/stats.py
@@ -0,0 +1,24 @@
+from typing import Dict, List
+from pydantic import BaseModel, Field
+
+
+class StatsChart(BaseModel):
+    cnt: int
+    diff: str
+    fit_diff: float
+    avg: float
+    avg_dx: float
+    std_dev: float
+    dist: List[int]
+    fc_dist: List[int]
+
+
+class StatsDiff(BaseModel):
+    achievements: float
+    dist: List[float]
+    fc_dist: List[float]
+
+
+class Stats(BaseModel):
+    charts: Dict[str, List[StatsChart]]
+    diff_data: Dict[str, StatsDiff]
