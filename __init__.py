0af83df4df08d6a4b1245d46e95939b7e5a66a3e
TTsdzb <ttsdzb@outlook.com>
2023-10-18 20:51:08 +0800

    feat: plugin metadata

--- __init__.py
+++ __init__.py
@@ -6,7 +6,9 @@ from .config import Config
 __plugin_meta = PluginMetadata(
     name="maimaidx-nonebot",
     description="移植自Hoshino的国服舞萌DX插件",
-    usage="",
+    usage="查询舞萌DX的成绩等数据。",
+    type="application",
+    homepage="https://github.com/TTsdzb/maimaidx-nonebot",
     config=Config,
 )
 
